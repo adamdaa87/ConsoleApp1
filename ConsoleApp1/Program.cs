a18b0c0843cc0e15f4514bf9fa7c6189799da0af
muhammadnaveedahmed <mnahmed786@gmail.com>
2022-07-07 09:58:27 +0200

    Git Lektion

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, First!");
 Console.WriteLine("Its a me, mario");
+Console.WriteLine("Its Muhammad's Team");
+Console.WriteLine("Good Morning, Welcome All of You");
+Console.WriteLine("Git Lektion");
 
